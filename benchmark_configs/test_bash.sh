b4cbac9808b1f7ffedf081fb2da52c3c6daecd43
Jin, Baihui <baihui.jin@intel.com>
2024-06-16 16:21:12 +0000

    push

--- benchmark_configs/test_bash.sh
+++ benchmark_configs/test_bash.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 # set -ex
 
-echo "param is $1"
-echo "param is $2"
-
-range=$1
-other=$2
-
-echo "param is ${range}"
-echo "param is ${other}"
-pro=$(( (range + other)*32 ))
-echo "${pro}"
+echo "TGI automatically bench"
