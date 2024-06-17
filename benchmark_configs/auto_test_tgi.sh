8562e6444ca7a43613a35a98e932143dabf01771
BaihuiJin <baihui.jin@intel.com>
2024-06-17 15:12:55 +0800

    push

--- benchmark_configs/auto_test_tgi.sh
+++ benchmark_configs/auto_test_tgi.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # set -x
 
-input_arr=(128 512 128)
-output_arr=(1024 256 768)
-range_arr=(1024 256 512)
+input_arr=(128 128 128 512 2048)
+output_arr=(1024 768 1536 768 96)
+range_arr=(1024 256 512 256 32)
 bs_arr=(16 32 64 128)
-dist_arr=("capped_exponential" "capped_exponential" "uniform")
+# input_arr=(128)
+# output_arr=(1024)
+# range_arr=(1024)
+# bs_arr=(32)
+dist_arr=("capped_exponential" "uniform" "uniform" "uniform" "uniform")
 for bs in "${bs_arr[@]}"
 do
     idx=0
@@ -17,6 +21,9 @@ do
         dist=${dist_arr[$idx]}
         echo "bs:$bs input:$input output:$output range:$range dist:$dist"
         bash ./tgi_gaudi_variable_size $input $output $range $bs $dist $dist 2>&1 | tee term_log_i${input}_o${output}_range${range}_bs${bs}
+        print_o=$(( output + range ))
+        print_r=$((range * 2))
+        # mv ../bsoutput_new.txt ./bs_modeltime/i${input}_o${print_o}_range${print_r}_bs${bs}.txt
         idx=$idx+1
     done
 done
