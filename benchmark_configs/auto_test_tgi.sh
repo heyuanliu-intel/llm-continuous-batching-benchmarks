aff88ffe0f9cae869c22f368dc70285a40630f5d
chaofan.li <chaofan.li@intel.com>
2024-06-20 03:01:29 +0000

    add model path dir

--- benchmark_configs/auto_test_tgi.sh
+++ benchmark_configs/auto_test_tgi.sh
@@ -6,14 +6,20 @@
 # in512 , out1-256
 # in1600-2048, out300-500
 # in800-1024, out300-500
+model_name_dir=/nfs/models--meta-llama--Llama-2-7b-hf/snapshots/8cca527612d856d7d32bd94f8103728d614eb852
 
-input_mean_arr=(128 512 1824 912)
-input_range_arr=(0 0 448 224)
-output_mean_arr=(768 256 400 400)
-output_range_arr=(512 256 200 200)
-bs_arr=(16 32 64 128)
+# input_mean_arr=(128 512 1824 912)
+# input_range_arr=(0 0 448 224)
+# output_mean_arr=(768 256 400 400)
+# output_range_arr=(512 256 200 200)
+# dist_arr=("uniform" "capped_exponential" "uniform" "uniform")
 
-dist_arr=("uniform" "capped_exponential" "uniform" "uniform")
+input_mean_arr=(912)
+input_range_arr=(224)
+output_mean_arr=(400)
+output_range_arr=(200)
+bs_arr=(16)
+dist_arr=("uniform")
 
 for bs in "${bs_arr[@]}"
 do
@@ -26,7 +32,7 @@ do
         output_range=${output_range_arr[$idx]}
         dist=${dist_arr[$idx]}
         echo "bs:$bs input_mean:$input_mean input_range:$input_range output_mean:$output_mean range:$output_range dist:$dist"
-        bash ./tgi_gaudi_variable_size $input_mean $output_mean $output_range $bs $dist $input_range 2>&1 | tee term_log_im${input_mean}_ir${input_range}_om${output_mean}_or${output_range}_bs${bs}
+        bash ./tgi_gaudi_variable_size $input_mean $output_mean $output_range $bs $dist $input_range $model_name_dir 2>&1 | tee term_log_im${input_mean}_ir${input_range}_om${output_mean}_or${output_range}_bs${bs}
         # print_o=$(( output + range ))
         # print_r=$((range * 2))
         # mv ../bsoutput_new.txt ./bs_modeltime/i${input}_o${print_o}_range${print_r}_bs${bs}.txt
