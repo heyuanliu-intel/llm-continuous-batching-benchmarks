c35d095846df4dab913a7b895c668a46ac0f91bd
chaofanli <chaofan.li@intel.com>
2024-06-20 13:19:06 +0000

    enable multi-card support

--- benchmark_configs/auto_test_tgi.sh
+++ benchmark_configs/auto_test_tgi.sh
@@ -6,20 +6,37 @@
 # in512 , out1-256
 # in1600-2048, out300-500
 # in800-1024, out300-500
-model_name_dir=/nfs/models--meta-llama--Llama-2-7b-hf/snapshots/8cca527612d856d7d32bd94f8103728d614eb852
 
+# llama2-7b
+# model_name_dir=/nfs/models--meta-llama--Llama-2-7b-hf/snapshots/8cca527612d856d7d32bd94f8103728d614eb852
+# model_name_dir=/models/llama2-70b-chat
+# num_shard=1
 # input_mean_arr=(128 512 1824 912)
 # input_range_arr=(0 0 448 224)
 # output_mean_arr=(768 256 400 400)
 # output_range_arr=(512 256 200 200)
 # dist_arr=("uniform" "capped_exponential" "uniform" "uniform")
+# # data_type=bfloat16
+# bs_arr=(16 32 64 128)
 
-input_mean_arr=(912)
-input_range_arr=(224)
-output_mean_arr=(400)
-output_range_arr=(200)
-bs_arr=(16)
-dist_arr=("uniform")
+# llama2-70b
+model_name_dir=/models/llama2-70b-chat
+num_shard=8
+input_mean_arr=(384 128 912)
+input_range_arr=(256 0 224)
+output_mean_arr=(384 912 128)
+output_range_arr=(256 224 0)
+dist_arr=("uniform" "uniform" "uniform")
+# data_type=bfloat16
+bs_arr=(16 32 64 128)
+
+# single case
+# input_mean_arr=(912)
+# input_range_arr=(224)
+# output_mean_arr=(400)
+# output_range_arr=(200)
+# bs_arr=(16)
+# dist_arr=("uniform")
 
 for bs in "${bs_arr[@]}"
 do
@@ -31,11 +48,8 @@ do
         output_mean=${output_mean_arr[$idx]}
         output_range=${output_range_arr[$idx]}
         dist=${dist_arr[$idx]}
-        echo "bs:$bs input_mean:$input_mean input_range:$input_range output_mean:$output_mean range:$output_range dist:$dist"
-        bash ./tgi_gaudi_variable_size $input_mean $output_mean $output_range $bs $dist $input_range $model_name_dir 2>&1 | tee term_log_im${input_mean}_ir${input_range}_om${output_mean}_or${output_range}_bs${bs}
-        # print_o=$(( output + range ))
-        # print_r=$((range * 2))
-        # mv ../bsoutput_new.txt ./bs_modeltime/i${input}_o${print_o}_range${print_r}_bs${bs}.txt
+        echo "bs:$bs input_mean:$input_mean input_range:$input_range output_mean:$output_mean range:$output_range dist:$dist card_num $num_shard"
+        bash ./tgi_gaudi_variable_size $input_mean $output_mean $output_range $bs $dist $input_range $model_name_dir $num_shard 2>&1 | tee term_log_im${input_mean}_ir${input_range}_om${output_mean}_or${output_range}_bs${bs}_cn${num_shard}
         idx=$idx+1
     done
 done
